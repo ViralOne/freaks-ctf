dc9cd0710d6640afc7522f2b733fe9defb9754ee
Mocanu Marian <viralnico@protonmail.com>
2023-06-18 16:01:54 +0300

    Fix folder structure

--- challenge-2/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CREATE USER 'admindb'@'localhost' IDENTIFIED BY 'Passw0rd';
-GRANT ALL PRIVILEGES ON workers.* TO 'admindb'@'localhost';
-FLUSH PRIVILEGES;
-SELECT user FROM mysql.user;
