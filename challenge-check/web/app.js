5f92e098437a192321f5d1fde6a331fae51766f7
Mocanu Marian <mocanu.marian@agilefreaks.com>
2023-06-23 20:27:54 +0300

    K8s WIP deploy file, tips, fixes

--- challenge-check/web/app.js
+++ challenge-check/web/app.js
@@ -17,35 +17,35 @@ const challenges = [
     id: 1,
     name: 'Challenge 1',
     description: 'Network Exploitation',
-    tips: 'ToDO',
+    tips: 'Save bedgeID, the long token, user:pass url & the ssh',
     validText: ['dIzW9htx1Ii5oR4qgoC12XC8bW070cxXBm7IXSTH0YwxKTZDXSlo7KGdy6qMmdL9', "AF0069", "admin", "f4k3Freak", "ssh -p 2222 worker@"]
   },
   {
     id: 2,
     name: 'Challenge 2',
     description: 'Vulnerable WebApp',
-    tips: 'ToDO',
+    tips: 'Use the most common sqli queries, LeakedDoc.txt its very important',
     validText: ['LeakedDoc.txt', 'sqli', 'SQLi']
   },
   {
     id: 3,
     name: 'Challenge 3',
     description: 'Use your brain',
-    tips: 'ToDO',
+    tips: 'Affine Cipher a & b each one has a number -> SAML -> Base64 -> MD5',
     validText: ['d41e98d1eafa6d6011d3a70f1a5b92f0', 'ZDQxZTk4ZDFlYWZhNmQ2MDExZDNhNzBmMWE1YjkyZjA=', 'BUAxCoAwDHxTxdVBubg1UBBK7wUhsXv09YVoySd24n5Hp+lsW4Ukoab/NWuXMjw++nks', 'FYEbGsEaHLbXbhZFyfk1YFFO7aYlwBz09CZscWh24r5Lt+pwA4Yosef/RAyBQna++row', 'Passw0rd']
   },
   {
     id: 4,
     name: 'Challenge 4',
     description: 'Social Engineering',
-    tips: 'ToDO',
+    tips: 'How do you exit vim',
     validText: [':q', 'vim', 'exit vim']
   },
   {
     id: 5,
     name: 'Challenge 5',
     description: 'Save the world',
-    tips: 'ToDO',
+    tips: 'I love cookies when I am an admin, Dashboard answer its in challenge 4, two long tokens and im done',
     validText: ['dIzW9htx1Ii5oR4qgoC12XC8bW070cxXBm7IXSTH0YwxKTZDXSlo7KGdy6qMmdL9', 'taSwYsvX53/HrM!eW3ir4TLPphWFHQ0kcCr=p/-k!dBkTkYeqa8T6l/BeMH/OThh', 'admin']
   }
 ];
