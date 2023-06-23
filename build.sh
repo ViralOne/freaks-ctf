e968dcf29a8a4a21c6875e6459de1193d36ae8a1
Mocanu Marian <mocanu.marian@agilefreaks.com>
2023-06-23 21:06:58 +0300

    Update build script

--- build.sh
+++ build.sh
@@ -1,33 +1,101 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-command -v docker >/dev/null 2>&1 || {
-    echo "Docker is not running. Please start Docker and try again."
-    exit 1
+# Display usage instructions
+print_usage() {
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  -h, --help       : Show usage instructions"
+  echo "  -b, --build      : Only build the Docker images"
+  echo "  -p, --push       : Build and push the Docker images to DockerHub"
+  echo "  -u, --username <username> : Set the DockerHub username"
 }
 
-command -v jq >/dev/null 2>&1 || {
-    echo "jq is not installed. Please install jq and try again."
-    exit 1
-}
-
-DOCKER_IMAGES=(
+FOLDERS=(
   "challenge-1"
   "challenge-2"
   "challenge-3"
   "challenge-4"
   "challenge-5"
+  "challenge-check"
 )
 
+# Authenticate with Docker Hub with local credentials
+docker_login() {
+  docker login
+}
 
-# Build function to build Docker images
+# Build the Docker images
 build_images() {
-  for directory in "${DOCKER_IMAGES[@]}"; do
-    echo "Building image in $directory..."
-    docker build -t "$directory" "$directory"
-    echo "Image built in $directory"
-    echo ""
+  for folder in "${FOLDERS[@]}"
+  do
+    dockerfiles=($(find "$folder" -type f -name "Dockerfile*"))
+    for dockerfile in "${dockerfiles[@]}"
+    do
+      tag="${folder#*-}"
+      additional_tag=""
+      if [[ "$dockerfile" == *".web" ]]; then
+        additional_tag="-web"
+      elif [[ "$dockerfile" == *".db" ]]; then
+        additional_tag="-db"
+      elif [[ "$dockerfile" == *".ssh" ]]; then
+        additional_tag="-ssh"
+      fi
+      image_tag="$USERNAME/challenge:$tag$additional_tag"
+      docker build -t "$image_tag" -f "$dockerfile" "$folder"
+    done
   done
 }
 
-# Call the build_images function
-build_images
+# Push the Docker images
+push_images() {
+  for folder in "${FOLDERS[@]}"
+  do
+    dockerfiles=($(find "$folder" -type f -name "Dockerfile*"))
+    for dockerfile in "${dockerfiles[@]}"
+    do
+      tag="${folder#*-}"
+      additional_tag=""
+      if [[ "$dockerfile" == *".web" ]]; then
+        additional_tag="-web"
+      elif [[ "$dockerfile" == *".db" ]]; then
+        additional_tag="-db"
+      elif [[ "$dockerfile" == *".ssh" ]]; then
+        additional_tag="-ssh"
+      fi
+      image_tag="$USERNAME/challenge:$tag$additional_tag"
+      docker push "$image_tag"
+    done
+  done
+}
+
+# Set username
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    -u|--username)
+      USERNAME="$2"
+      shift 2
+      ;;
+    -h|--help)
+      print_usage
+      exit 0
+      ;;
+    -p|--push)
+      docker_login
+      build_images
+      push_images
+      exit 0
+      ;;
+    -b|--build)
+      build_images
+      exit 0
+      ;;
+    *)
+      echo "Invalid option: $1"
+      print_usage
+      exit 1
+      ;;
+  esac
+done
+
+# If no valid option is provided, print usage instructions
+print_usage
