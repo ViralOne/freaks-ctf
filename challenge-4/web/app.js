876bdc7363b6c67b8907c22530a890bcad991c4e
Mocanu Marian <viralnico@protonmail.com>
2023-07-01 21:05:52 +0300

    Fix check page & fix story small parts

--- challenge-4/web/app.js
+++ challenge-4/web/app.js
@@ -68,7 +68,7 @@ app.get('/chat', (req, res) => {
     messageCount++;
   } else {
     // Check if the message matches specific conditions
-    if (message.includes(':q')) {
+    if (message.includes(':q') || message.includes(':wq')) {
       res.send("Oooooooh, that was it? It's been 2 weeks since I opened the window, thanks! By the way, check this URL that I found http://192.168.0.248:6374 when looking through files, I think it's the nuclear reactor control interface.");
     } else if (message.includes('help')) {
       res.send("I don't know if that helps you, but you could pay attention to the status from the XIPE Corp Nuclear Plant website");
--- challenge-check/web/app.js
+++ challenge-check/web/app.js
@@ -1,95 +1,136 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-
-// Create an express app
 const app = express();
 
-// Middleware for parsing JSON body
-app.use(bodyParser.json());
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-// Store the CTF challenges and their valid texts
+// Parse JSON bodies
+app.use(express.json());
+
+// Sample valid texts
 const challenges = [
   {
     id: 1,
-    name: 'Challenge 1',
     description: 'Network Exploitation',
-    tips: 'Save bedgeID, the long token, user:pass url & the ssh',
-    validText: ['dIzW9htx1Ii5oR4qgoC12XC8bW070cxXBm7IXSTH0YwxKTZDXSlo7KGdy6qMmdL9', "AF0069", "admin", "f4k3Freak", "ssh -p 2222 worker@"]
+    tips: 'Save bedgeID, the long token, user:pass URL & the SSH',
+    validText: [
+      'dIzW9htx1Ii5oR4qgoC12XC8bW070cxXBm7IXSTH0YwxKTZDXSlo7KGdy6qMmdL9', 'AF0069', 'YWRtaW4=', 'ZjRrM0ZyZWFr', 'admin', 'f4k3Freak'
+    ]
   },
   {
     id: 2,
-    name: 'Challenge 2',
     description: 'Vulnerable WebApp',
-    tips: 'Use the most common sqli queries, LeakedDoc.txt its very important',
-    validText: ['LeakedDoc.txt', 'sqli', 'SQLi']
+    tips: 'Use the most common SQLi queries, LeakedDoc.txt is very important',
+    validText: [
+      'LeakedDoc.txt', 'sqli', 'buliver', 'modchat', 'M0r3Lor3'
+    ]
   },
   {
     id: 3,
-    name: 'Challenge 3',
     description: 'Use your brain',
     tips: 'Affine Cipher a & b each one has a number -> SAML -> Base64 -> MD5',
-    validText: ['d41e98d1eafa6d6011d3a70f1a5b92f0', 'ZDQxZTk4ZDFlYWZhNmQ2MDExZDNhNzBmMWE1YjkyZjA=', 'BUAxCoAwDHxTxdVBubg1UBBK7wUhsXv09YVoySd24n5Hp+lsW4Ukoab/NWuXMjw++nks', 'FYEbGsEaHLbXbhZFyfk1YFFO7aYlwBz09CZscWh24r5Lt+pwA4Yosef/RAyBQna++row', 'Passw0rd']
+    validText: [
+      'd41e98d1eafa6d6011d3a70f1a5b92f0', 'ZDQxZTk4ZDFlYWZhNmQ2MDExZDNhNzBmMWE1YjkyZjA=', 'BUAxCoAwDHxTxdVBubg1UBBK7wUhsXv09YVoySd24n5Hp+lsW4Ukoab/NWuXMjw++nks', 'FYEbGsEaHLbXbhZFyfk1YFFO7aYlwBz09CZscWh24r5Lt+pwA4Yosef/RAyBQna++row', 'Passw0rd'
+    ]
   },
   {
     id: 4,
-    name: 'Challenge 4',
     description: 'Social Engineering',
-    tips: 'How do you exit vim',
-    validText: [':q', 'vim', 'exit vim']
+    tips: 'How do you exit Vim?',
+    validText: [
+      ':q', ':wq', 'vim', 'exit Vim', 'AF0069', 'Passw0rd', 'd22fc547518b36f7ba26acc8cd2f973c'
+    ]
   },
   {
     id: 5,
-    name: 'Challenge 5',
     description: 'Save the world',
-    tips: 'I love cookies when I am an admin, Dashboard answer its in challenge 4, two long tokens and im done',
-    validText: ['dIzW9htx1Ii5oR4qgoC12XC8bW070cxXBm7IXSTH0YwxKTZDXSlo7KGdy6qMmdL9', 'admin']
+    tips: 'I love cookies when I am an admin. Dashboard answer is in challenge 4. Two long tokens, and I\'m done',
+    validText: [
+      'dIzW9htx1Ii5oR4qgoC12XC8bW070cxXBm7IXSTH0YwxKTZDXSlo7KGdy6qMmdL9', 'admin', 'd22fc547518b36f7ba26acc8cd2f973c', 'taSwYsvX53/HrM!eW3ir4TLPphWFHQ0kcCr=p/-k!dBkTkYeqa8T6l/BeMH/OThh', 'cookie', 'worker', 'tempassw0rd'
+    ]
   },
   {
     id: 6,
-    name: 'Challenge 6',
-    description: 'Last token - needs to be completed to finish challenge 5',
-    tips: 'Try different directories & see what can you do with: ; cat',
-    validText: ['taSwYsvX53/HrM!eW3ir4TLPphWFHQ0kcCr=p/-k!dBkTkYeqa8T6l/BeMH/OThh']
+    description: 'Last token',
+    tips: 'Try different directories & see what you can do with: ; cat',
+    validText: [
+      'taSwYsvX53/HrM!eW3ir4TLPphWFHQ0kcCr=p/-k!dBkTkYeqa8T6l/BeMH/OThh'
+    ]
   }
 ];
 
-// Route for getting all challenges
-app.get('/challenges', (req, res) => {
-  res.json(challenges);
-});
-
-// Route for checking the validity of a challenge text
+// Handle the answer validation
 app.post('/check', (req, res) => {
-  const { challengeId, text, showTips } = req.body;
-  const challenge = challenges.find(challenge => challenge.id === challengeId);
+  const answer = req.body.answer;
+  const showTips = req.body.tips === true;
 
-  if (!challenge) {
-    res.status(404).json({ error: 'Challenge not found' });
-    return;
+  const validChallenges = [];
+
+  for (const challenge of challenges) {
+    const validTextIndex = challenge.validText.findIndex(
+      validText => validText.toLowerCase() === answer.toLowerCase()
+    );
+
+    if (validTextIndex !== -1) {
+      const validChallenge = {
+        id: challenge.id,
+        description: challenge.description
+      };
+
+      if (showTips) {
+        validChallenge.tips = challenge.tips;
+      }
+
+      validChallenges.push(validChallenge);
+    }
   }
 
-  // Perform validation logic
-  const isValid = challenge.validText.includes(text);
+  if (validChallenges.length === 1) {
+    const response = {
+      message: `Congratulations! Your answer "${answer}" is valid for challenge ${validChallenges[0].id}: "${validChallenges[0].description}".`
+    };
 
-  // Prepare the response object
-  const response = {
-    valid: isValid
-  };
+    if (showTips && validChallenges[0].tips) {
+      response.tips = validChallenges[0].tips;
+    }
 
-  if (showTips && isValid) {
-    response.tips = challenge.tips;
+    res.json(response);
+  } else if (validChallenges.length > 1) {
+    const challengeMessage = validChallenges
+      .map(challenge => `${challenge.id} - ${challenge.description}`)
+      .join(', ');
+
+    const response = {
+      message: `Congratulations! Your answer "${answer}" is valid and was found in multiple challenges: ${challengeMessage}.`
+    };
+
+    if (showTips) {
+      const tips = validChallenges
+        .map(challenge => challenge.tips)
+        .filter(tips => !!tips);
+
+      if (tips.length > 0) {
+        response.tips = tips;
+      }
+    }
+
+    res.json(response);
+  } else {
+    res.json({
+      message: 'Sorry, your answer is not valid.'
+    });
   }
-
-  res.json(response);
 });
 
+// Render the index.ejs template
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 // Start the server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
